71db024fd2ab5a2d57dd6efb84f4610d5bd78c7e
Aditya Chandra <adityachandra9898@gmail.com>
2021-11-05 12:22:29 +0530

    Added mongoose hooks

--- models/User.js
+++ models/User.js
@@ -15,7 +15,18 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'Minimum password length is 6 characters']
     },
 });
+//after save to db
+userSchema.post('save', (doc, next) => {
+    console.log('new user was created and saved', doc);
+    next();
+} )
 
+
+//fire before saving to doc
+userSchema.pre('save', (next) =>{
+    console.log("User about to be created and saved", this);
+    next();
+})
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ const router = Router();
 router.get('/signup', authController.signup_get );
 router.post('/signup', authController.signup_post);
 router.get('/login', authController.login_get );
-router.post('/login', authController.login_post );
+router.post('/login', authController.login_post);
+router.post('/login', authController.login_post);
 
 module.exports = router;
